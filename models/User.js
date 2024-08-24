13fca43e634f8fadc390d0e5b02a241c5a4b0415
bhargav-tibadiya <bhargavptibadiya.tbp@gmail.com>
2024-08-24 10:54:57 +0530

    User Model Updated

---
+++ models/User.js
@@ -0,0 +1,55 @@
+// --> Importing All Dependancy <--
+const mongoose = require('mongoose');
+
+
+// --> Defining Schema for User <--
+const userSchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  accountType: {
+    type: String,
+    enum: ["Admin", "Student", "Instructor"],
+    required: true,
+  },
+  additionalDetails: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: "Profile"
+  },
+  courses: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Course"
+    }
+  ],
+  image: {
+    type: String,
+    required: true,
+  },
+  courseProgress: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "CourseProgress",
+    }
+  ]
+})
+
+// --> Exporting Schema as User <--
+module.exports = mongoose.model("User", userSchema)
