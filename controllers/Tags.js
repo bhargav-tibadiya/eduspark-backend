651c0592e2402e445dfb1284b234f204b6bc84f3
bhargav-tibadiya <bhargavptibadiya.tbp@gmail.com>
2024-09-05 21:40:09 +0530

    Conroller: Show All Tags Complete

--- controllers/Tags.js
+++ controllers/Tags.js
@@ -47,4 +47,33 @@ exports.createTag = async (req, res) => {
     throw error;
 
   }
-} 
+}
+
+
+// Handler Funcction to fetch All Tags
+exports.showAllTags = async (req, res) => {
+  try {
+
+    // Fetch All data from DB
+    const allTags = await Tag.find({}, { name: true, description: true })
+
+    // return Response
+    res.status(200).json({
+      sucess: true,
+      message: "All Tags Fetched Successfully ",
+      allTags: allTags
+    })
+
+  } catch (error) {
+
+    res.status(500).json({
+      sucess: false,
+      message: "Error While Fetching All Tag",
+    })
+
+    console.log("Error While Fetching All Tag. \nCheck Tags.js File #BE034");
+    console.error(error.message);
+    throw error;
+
+  }
+}
